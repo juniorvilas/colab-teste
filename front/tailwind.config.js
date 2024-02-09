8bd7b5bc9d69940f52410b3c42784ffd1c96217d
juniorvilas <log_junior@hotmail.com>
2024-02-09 15:28:12 -0300

    chore: change font-family and colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,10 +4,20 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: "Roboto, sans-serif",
+        sans: "Montserrat, sans-serif",
       },
       colors: {
-        primary: "#1FDF64",
+        primary: {
+          100: "#caf0f8",
+          200: "#ade8f4",
+          300: "#90e0ef",
+          400: "#48cae4",
+          500: "#00b4d8",
+          600: "#0096c7",
+          700: "#0077b6",
+          800: "#023e8a",
+          900: "#03045e",
+        },
       },
     },
   },
