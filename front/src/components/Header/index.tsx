37ae6138d9906f2c9bf3ba998dab158642e571b6
juniorvilas <log_junior@hotmail.com>
2024-02-08 22:37:26 -0300

    feat: create new component Header

---
+++ front/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <>
+      <header className="w-full items-center justify-center flex mx-auto z-20 py-6 select-none bg-cyan-50  shadow-lg">
+        <div className="container relative max-w-[1280px] flex mx-auto items-center justify-center px-2 overflow-hidden">
+          <h1 className="text-cyan-950 text-4xl font-semibold">Radom User</h1>
+        </div>
+      </header>
+    </>
+  );
+}
