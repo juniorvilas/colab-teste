f11787570a532bdcd1804b8225dfe9f18d149ce1
juniorvilas <log_junior@hotmail.com>
2024-02-08 22:35:16 -0300

    feat: init new project front with vite

---
+++ front/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+      colors: {
+        primary: "#1FDF64",
+      },
+    },
+  },
+  plugins: [],
+};
