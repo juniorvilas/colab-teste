2153dee1508eabaaf1585d54c2163aa30d201a65
juniorvilas <log_junior@hotmail.com>
2024-02-08 22:37:40 -0300

    feat: create new component Layout

---
+++ front/src/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
