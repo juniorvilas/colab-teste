55c8607c2f5a94b39d6bda80ca5ff0cc8aad8fd0
juniorvilas <log_junior@hotmail.com>
2024-02-08 22:37:15 -0300

    feat: create new component Footer

---
+++ front/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className="text-sm w-full fixed bottom-0 font-body text-cyan-200">
+      <div className="bg-ming-50 text-swamp-950/90">
+        <div className="container py-4 mx-auto text-center">
+          <h2 className="text-sm lg:text-base">
+            Feito com ❤️ por Carlindo Junior 2024
+          </h2>
+        </div>
+      </div>
+    </footer>
+  );
+}
