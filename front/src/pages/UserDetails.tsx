d06981b4b377e853f9f33e52f6b98007f44ff03f
juniorvilas <log_junior@hotmail.com>
2024-02-09 19:43:48 -0300

    style: change style layout of page user details

--- front/src/pages/UserDetails.tsx
+++ front/src/pages/UserDetails.tsx
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import toast from "react-hot-toast";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
 import { MdOutlineArrowBackIos } from "react-icons/md";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
@@ -31,7 +32,7 @@ export function UserDetails() {
           </Link>
         </div>
 
-        <div className="container flex flex-col items-center px-2 max-w-[1280px]">
+        <div className="container flex flex-col items-center px-2 max-w-[650px]">
           <h1 className="mb-4 text-3xl lg:text-4xl">Profile</h1>
           <h1>{shortbio}</h1>
           <div className="rounded-md py-6 flex items-center w-full justify-center">
@@ -59,49 +60,52 @@ export function UserDetails() {
 
           {/* LOCATION */}
 
-          <div className="text-swamp items-center justify-center flex flex-col gap-10">
-            <h1 className="font-semibold italic text-xl lg:text-3xl">
+          <div className="text-swamp w-full flex flex-col gap-10">
+            <h1 className="font-semibold italic text-3xl lg:text-4xl">
               Details
             </h1>
 
-            <div className="flex items-center justify-center flex-col gap-3">
-              <div className="flex items-center justify-center flex-col gap-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div className="flex flex-col gap-2">
                 <h2 className="font-semibold">Age:</h2>
                 <h2 className="text-primary-500">{userDetails.dob.age}</h2>
               </div>
-              <div className="flex items-center justify-center flex-col gap-2">
+              <div className="flex flex-col gap-2">
                 <h2 className="font-semibold">Gender:</h2>
                 <h2 className="text-primary-500">{userDetails.gender}</h2>
               </div>
-              <div className="flex items-center justify-center flex-col gap-2">
+              <div className="flex flex-col gap-2">
                 <h2 className="font-semibold">Phone:</h2>
-
-                <Tooltip placement="left-start" arrow title="Copy number">
-                  <button
-                    onClick={handlePhoneClick}
-                    className="cursor-pointer text-primary-500 hover:text-primary-200 flex items-center gap-3"
-                  >
-                    <h2>{userDetails.phone}</h2>
-                  </button>
-                </Tooltip>
+                <div className="flex items-center">
+                  <Tooltip placement="left-start" arrow title="Copiar número">
+                    <button
+                      onClick={handlePhoneClick}
+                      className="cursor-pointer group text-primary-500 hover:text-primary-200 flex items-center gap-1"
+                    >
+                      <span className="mr-1">{userDetails.phone}</span>
+                      <FaPhone className="group-hover:translate-x-2 transition ease-in" />
+                    </button>
+                  </Tooltip>
+                </div>
               </div>
-              <div className="flex items-center justify-center flex-col gap-2">
+              <div className="flex flex-col gap-2">
                 <h2 className="font-semibold">Email:</h2>
                 <a
                   target="_blank"
                   type="button"
                   href={`mailto:${userDetails.email}`}
-                  className=" w-full flex items-center text-primary-500 hover:text-primary-200 gap-2  justify-center  font-medium text-sm py-2.5 text-center me-2 mb-2"
+                  className="flex items-center group text-primary-500 hover:text-primary-200 gap-1"
                 >
-                  {userDetails.email}
+                  <span>{userDetails.email}</span>
+                  <FaEnvelope className="group-hover:translate-x-2 transition ease-in" />
                 </a>
               </div>
-              <div className="flex items-center justify-center flex-col gap-2">
+              <div className="flex flex-col gap-2">
                 <h2 className="font-semibold">Location:</h2>
                 <h2 className="text-primary-500">
                   {userDetails.location.street.name},{" "}
                   {userDetails.location.street.number}{" "}
-                  {userDetails.location.state}-{userDetails.location.country}
+                  {userDetails.location.state}, {userDetails.location.country}
                 </h2>
               </div>
             </div>
