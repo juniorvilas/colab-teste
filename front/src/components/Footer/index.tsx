fa0a0602d85eabfb48c5d0a1aa8b91396fbdba47
juniorvilas <log_junior@hotmail.com>
2024-02-09 15:30:59 -0300

    refactor: change colors

--- front/src/components/Footer/index.tsx
+++ front/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="text-sm w-full fixed bottom-0 font-body text-cyan-200">
+    <footer className="mt-5 text-sm w-full bottom-0 font-body text-primary-200">
       <div className="bg-ming-50 text-swamp-950/90">
         <div className="container py-4 mx-auto text-center">
           <h2 className="text-sm lg:text-base">
--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 export function Header() {
   return (
     <>
-      <header className="w-full items-center justify-center flex mx-auto z-20 py-6 select-none bg-cyan-50  shadow-lg">
+      <header className="w-full items-center justify-center flex mx-auto z-20 py-6 select-none bg-primary-100  shadow-lg">
         <div className="container relative max-w-[1280px] flex mx-auto items-center justify-center px-2 overflow-hidden">
-          <h1 className="text-cyan-950 text-4xl font-semibold">Radom User</h1>
+          <h1 className="text-primary-900 text-3xl lg:text-4xl font-semibold">
+            Random User
+          </h1>
         </div>
       </header>
     </>
